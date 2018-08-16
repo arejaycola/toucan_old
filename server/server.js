79e43a79c88ab8fadde3b482726a80986c492bc9
unknown <arejaycola@gmail.com>
2018-08-15 21:41:00 -0400

    Imports picture, name, and handle. Some basic formatting.

--- server/server.js
+++ server/server.js
@@ -1,45 +1,49 @@
-const express = require('express');
-const hbs = require('hbs');
 const fs = require('fs');
-const bodyParser = require('body-parser');
 const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const colors = require('colors');
 
 const {searchForVerifiedUser} = require('./TwitterHelper');
 
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 80; 
 
 var app = express();
-hbs.registerPartials(path.join(__dirname,  'views', 'partials'));
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+
+app.use(express.static(publicPath));
+
+
 app.set('view engine', 'hbs');
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-hbs.registerHelper('getCurrentYear', () =>{
-	return new Date().getFullYear();
-});
 
 app.post('/server/twitter-search', async (req, res) => {
 	try{
+
 		var user = await searchForVerifiedUser(req.body);
-		console.log(user.name);
+		fs.writeFileSync('results.json', JSON.stringify(user, undefined, 2));
+		user.profile_image_url_https = user.profile_image_url_https.replace(new RegExp('_normal', 'g'), '')
+		res.render('index', {
+			user:user
+		})
+		// res.renderPartials({
+		// 	result: {data: "user"}
+		// });
+		//res.render('partials/result', {layout: false, user: user})
 	}catch(e){
 		console.log(colors.red(e));
 	}
-	// searchForVerifiedUser(req.body).then((result) => {
-	// })
-	// .catch((error) => {
-	// 	console.log(colors.red(error));
-	// });
-})
+});
 
 app.get('/', (req, res) => {
-	res.render('index', {
-		welcomeMessage: 'Hello, welcome to my page',
-		pageTitle: 'Home Page'
-	});
+	res.render('index');
 });
 
 
