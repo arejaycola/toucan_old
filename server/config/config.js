5da8c8db91e5ef24570da846fd10696b876791fa
unknown <arejaycola@gmail.com>
2018-08-21 18:39:13 -0400

    Trying to fix config issues.

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,8 @@
 var env = process.env.NODE_ENV || 'development';
-if(env === 'development' || env === 'test'){
-	var config = require('./config.json');
-console.log(config);
-	var envConfig = config[env];
 
-	Object.keys(envConfig).forEach((key) => {
-		process.env[key] = envConfig[key];
-	});
+if(env === 'development' || env === 'test'){
+	process.env.CONSUMER_KEY = "YMWWT7q6Lwb9OZ0ce6RWbcLxe";
+	process.env.CONSUMER_SECRET = "A3qQ0HwGr3zXBJn8mODchzfGp6xkT6JRGoXe2dIIlpeGBZiD4N";
+	process.env.ACCESS_TOKEN_KEY = "18616681-HT2dhrDgTg7mW39iWrO65mqeUiu1PFgbKZ7tRT3Ig";
+	process.env.ACCESS_TOKEN_SECRET = "iwk6zVGvXqjJE69a2mBpa0qNc5LFBcvrzB0emXV9zoSOO";
 }
